329c4c691b5048c33533309fa9a43c16603bf39d
santiRedondo <santi16redo@gmail.com>
2023-08-14 21:51:22 -0300

    HACIENDO AJUSTES DE ESTILOS EN EL FORM

--- client/src/views/Form/Form.jsx
+++ client/src/views/Form/Form.jsx
@@ -1,14 +1,92 @@
 
 
+import React from 'react';
+import { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from "react-redux";
+import Nav from '../../components/Nav/Nav'
 
-import { useState } from 'react'
 import './form.css'
 import style from './form.module.css'
-import Nav from '../../components/Nav/Nav'
+import { getAllCountries } from '../../redux/action';
 
 
 export default function Form() {
 
+
+    const countries = useSelector(state => state.allCountries)
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(getAllCountries())
+    })
+
+    const [form, setForm] = useState({
+        name:'',
+        difficulty:'',
+        duration:'',
+        season:[],
+        Countries:[]
+    })
+
+    const handleChange = (event) => {
+
+        console.log(countries)
+
+        setForm({
+            ...form,
+            [event.target.name]: event.target.value
+        })
+    }
+
+    const handleDifficulty = (event) => {
+
+        setForm({
+            ...form,
+            difficulty: event.target.innerText
+        })
+
+    }
+
+    const handleSeason = (event) => {
+        console.log(form.season)
+        if(form.season.length > 1){
+            setForm({
+                ...form,
+                season: [event.target.innerText]
+            })
+            alert('Only 2 seasons are available at the same time')
+        } else{
+            setForm({
+                ...form,
+                season: [...form.season, event.target.innerText]
+            })
+        }
+
+    }
+
+    const handleCountry = (event) => {
+        console.log(form.Countries)
+
+        if(form.Countries.includes(event.target.innerText)){
+            return alert('That country has already been choosen')
+        }
+
+        if(form.Countries.length > 4){
+            alert('Only 5 countries are available at the same time')
+            setForm({
+                ...form,
+                Countries: [event.target.innerText]
+            })
+        } else {
+            setForm({
+                ...form,
+                Countries: [...form.Countries, event.target.innerText]
+            })
+        }
+    }
+
+
+    // FUNCIONES PARA LOS SELECTS
 
     const [difficulty, setDifficulty] = useState(false)
     const [season, setSeason] = useState(false)
@@ -92,7 +170,7 @@ export default function Form() {
             <form action="">
                 <div className='selectBox'>
                     <label>Name</label>
-                    <input type="text" name="" id="" />                    
+                    <input type="text" name="name" id="name" value={form.name} onChange={handleChange}/>                    
                 </div>
                 <div className='selectBox'>
                     <div onClick={handleSelectDifficulty} className='difficulty'>
@@ -100,28 +178,24 @@ export default function Form() {
                             <p className='title'>Difficulty</p>
                         </div>
                     </div>
-                    <div className={style.options}>1</div>
+                    <div id='difficulty' name='difficuulty' value={form.difficulty} onChange={handleChange} className={style.options}>{form.difficulty}</div>
                     <div className='hiddenOptions' id="difficultyOptions">
                         <div className="option">
-                            <p /* onClick={handleOrder} */ value="1">1</p>
+                            <p onClick={handleDifficulty}>1</p>
                         </div>
                         <div className="option">
-                            <p /* onClick={handleOrder} */ value="2">2</p>
+                            <p onClick={handleDifficulty}>2</p>
                         </div>
                         <div className="option">
-                            <p /* onClick={handleOrder} */ value="3">3</p>
+                            <p onClick={handleDifficulty}>3</p>
                         </div>
                         <div className="option">
-                            <p /* onClick={handleOrder} */ value="4">4</p>
+                            <p onClick={handleDifficulty}>4</p>
                         </div>
                         <div className="option">
-                            <p /* onClick={handleOrder} */ value="5">5</p>
+                            <p onClick={handleDifficulty}>5</p>
                         </div>
                     </div>
-                </div>
-                <div className='selectBox'>
-                    <label>Duration (hs) </label>
-                    <input type="text" name="" id="" />                    
                 </div>
                 <div className='selectBox'>
                     <div onClick={handleSelectSeason} className='season'>
@@ -129,19 +203,25 @@ export default function Form() {
                             <p className='title'>Season</p>
                         </div>
                     </div>
-                    <div className={style.options}>Summer</div>
+                    <div id='season' name='season' value={form.season} onChange={handleChange} className={style.options}>
+                        {
+                            form.season.map(season => {
+                                return form.season[0] === season ? `${season}, ` : `${season}`
+                            })
+                        }
+                    </div>
                     <div className='hiddenOptions' id="seasonOptions">
                         <div className="option">
-                            <p /* onClick={handleOrder} */ value="Summer">Summer</p>
+                            <p onClick={handleSeason} >Summer</p>
                         </div>
                         <div className="option">
-                            <p /* onClick={handleOrder} */ value="Autumn">Autumn</p>
+                            <p onClick={handleSeason} >Autumn</p>
                         </div>
                         <div className="option">
-                            <p /* onClick={handleOrder} */ value="Winter">Winter</p>
+                            <p onClick={handleSeason} >Winter</p>
                         </div>
                         <div className="option">
-                            <p /* onClick={handleOrder} */ value="Springs">Springs</p>
+                            <p onClick={handleSeason} >Springs</p>
                         </div>
                     </div>
                 </div>
@@ -151,21 +231,33 @@ export default function Form() {
                             <p className='title'>Countries</p>
                         </div>
                     </div>
-                    <div className={style.options}>Argentine</div>
                     <div className='hiddenOptions' id="countryOptions">
-                        <div className="option">
-                            <p /* onClick={handleOrder} */ value="Summer">Summer</p>
-                        </div>
-                        <div className="option">
-                            <p /* onClick={handleOrder} */ value="Autumn">Autumn</p>
-                        </div>
-                        <div className="option">
-                            <p /* onClick={handleOrder} */ value="Winter">Winter</p>
-                        </div>
-                        <div className="option">
-                            <p /* onClick={handleOrder} */ value="Springs">Springs</p>
-                        </div>
+                        {
+                            countries.map(country => {
+                                return(
+                                    <div key={country.name} className='option'>
+                                        <p onClick={handleCountry}>{country.name}</p>
+
+                                    </div>
+                                )
+                            })
+                        }
                     </div>
+                </div>
+                <div id='Countries' name='Countries' value={form.Countries} onChange={handleChange} className={style.countries}>
+                    {
+                        form.Countries.map(country => {
+                            return (
+                                <React.Fragment key={country}>
+                                    {form.Countries[4] !== country ? <>{country},<br/></> : `${country}`}
+                                </React.Fragment>
+                            )
+                        })
+                    }
+                </div>
+                <div className='selectBox'>
+                    <label>Duration (hs)</label>
+                    <input  type="text" name="duration" id="duration" value={form.duration} onChange={handleChange}/>                    
                 </div>
                 <div>
                     <button type='submit' className={style.button}>CREATE</button>
