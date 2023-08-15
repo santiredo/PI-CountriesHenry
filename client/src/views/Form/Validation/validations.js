88dbe5b2f07f31c9a31c4e713b468de30a1da0c8
santiRedondo <santi16redo@gmail.com>
2023-08-15 15:58:14 -0300

    CREANDO VALIDACIONES

---
+++ client/src/views/Form/Validation/validations.js
@@ -0,0 +1,40 @@
+
+
+export const validateForm = ({name, difficulty, season, Countries, duration}) => {
+
+    let errors = {}
+    let nameRegex = /^[a-zA-Z\s]+$/;
+
+    let durationRegex = /^[0-9]+$/
+    
+    if(name && !nameRegex.test(name)){
+        errors.name = 'This field accepts only letters'
+    }
+    if(duration && !name){
+        errors.name = 'This field is required'
+    }
+    if(duration && !durationRegex.test(duration)){
+        errors.duration = 'This field accepts only numbers'
+    }
+    if(duration && !difficulty){
+        errors.difficulty = 'This field is required'
+    }
+    if(duration && !season){
+        errors.season = 'This field is required'
+    }
+    if(duration && Countries.length === 0){
+        errors.Countries = 'This field is required'
+    }
+
+    return errors
+}
+
+export const validateSubmit = ({name, difficulty, season, Countries, duration}) => {
+    let errors = false
+
+    if(!name || !difficulty || !season || !Countries || !duration){
+        errors = true
+    }
+
+    return errors
+}
