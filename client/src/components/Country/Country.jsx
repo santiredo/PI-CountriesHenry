9b7303d3de5fea6252e82decd05772990e66b8c8
santiRedondo <santi16redo@gmail.com>
2023-08-16 20:16:18 -0300

    PASANDO COMPONENTE A TIPO CLASE

--- client/src/components/Country/Country.jsx
+++ client/src/components/Country/Country.jsx
@@ -1,23 +1,29 @@
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+import style from './country.module.css';
 
-import {NavLink} from 'react-router-dom';
-import style from './country.module.css'
+class Country extends Component {
+    render() {
+        const { id, name, image, continent } = this.props;
 
-export default function Country({id, name, image, continent}) {
-    return (
-        <div className={style.countryCard}>
-            <img className={style.background} src={image} alt="" />
-            <div className={style.cardInfo}>
-                <div className={style.imageDiv}>
-                    <div>
-                        <img src={image} alt="" />
-                        <NavLink to={`/detail/${id}`}>+INFO</NavLink>
+        return (
+            <div className={style.countryCard}>
+                <img className={style.background} src={image} alt="" />
+                <div className={style.cardInfo}>
+                    <div className={style.imageDiv}>
+                        <div>
+                            <img src={image} alt="" />
+                            <NavLink to={`/detail/${id}`}>+INFO</NavLink>
+                        </div>
+                    </div>
+                    <div className={style.countryDetails}>
+                        <h3>{name}</h3>
+                        <p>{continent}</p>
                     </div>
                 </div>
-                <div className={style.countryDetails}>
-                    <h3>{name}</h3>
-                    <p>{continent}</p>
-                </div>
             </div>
-        </div>
-    )
+        );
+    }
 }
+
+export default Country;
