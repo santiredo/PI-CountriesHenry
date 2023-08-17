bcaa52b3786f832aa2df368284ed132e06ca83ec
santiRedondo <santi16redo@gmail.com>
2023-08-16 21:19:53 -0300

    AGREGANDO BOTON PARA LA RUTA ACTIVITIES

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -2,6 +2,7 @@
 import {NavLink} from 'react-router-dom';
 import home from '../../assets/home.png'
 import create from '../../assets/create.png'
+import activities from '../../assets/activities.png'
 import style from './nav.module.css'
 
 
@@ -11,6 +12,7 @@ export default function() {
         <div className={style.nav}>
             <NavLink to={`/home`}><img src={home} alt="" /></NavLink>
             <NavLink to={`/form`}><img src={create} alt="" /></NavLink>
+            <NavLink to={'/activities'}><img src={activities} alt="" /></NavLink>
         </div>
     )
 }
