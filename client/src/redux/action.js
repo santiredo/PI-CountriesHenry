3c9b9cce208fe4c220a68868d659a558b453191b
santiRedondo <santi16redo@gmail.com>
2023-08-18 17:46:58 -0300

    PASANDO A PROMISE UNA ACTION

--- client/src/redux/action.js
+++ client/src/redux/action.js
@@ -1,15 +1,14 @@
 import axios from 'axios'
 
 export const getAllCountries = () => {
-    return async (dispatch) => {
+    return (dispatch) => {
         try {
-            const response = await axios('http://localhost:3001/countries')
-            const data = response.data
-            return dispatch({
-                type: 'GET_ALL_COUNTRIES',
-                payload: data
+            const response = axios('http://localhost:3001/countries').then(({data}) => {
+                return dispatch({
+                    type: 'GET_ALL_COUNTRIES',
+                    payload: data
+                })
             })
-            
         } catch (error) {
             alert(error.message)
         }
