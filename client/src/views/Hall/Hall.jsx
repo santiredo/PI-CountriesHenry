cc0de02439846385e4f29188ff9e6c18f0d44124
santiRedondo <santi16redo@gmail.com>
2023-10-02 20:43:54 -0300

    Inicio de la aplicacion de 0auth de google

--- client/src/views/Hall/Hall.jsx
+++ client/src/views/Hall/Hall.jsx
@@ -1,11 +1,25 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { gapi } from 'gapi-script'
+import GoogleLogin from 'react-google-login'
 import style from './hall.module.css';
 
 export default function Hall () {
 
+    const clientID = '295685867400-74qe6ef395rqe5f6nbnlse6dkq505qmv.apps.googleusercontent.com'
+
+    useEffect(() => {
+        const start = () => {
+            gapi.auth2.init({
+                clientId: clientID
+            })
+        }
+
+        gapi.load('client:auth2', start)
+    }, [])
+
     const [form, setForm] = useState({
-        username: null,
-        email: null
+        username: '',
+        email: ''
     })
 
     const handleChange = (event) => {
@@ -14,25 +28,57 @@ export default function Hall () {
             [event.target.name]: event.target.value
         })
     }
+
+    const [login, setLogin] = useState(true)
+
+    const handleLogin = () =>{
+        setLogin(true)
+    }
+
+    const handleRegister = () => {
+        setLogin(false)
+    }
   
     return (
       <div className={style.hallPage}>
-        <div className={style.divForm}>
-            <form className={style.registerForm}>
-                <div className={style.formDiv}>
-                    <div className={style.label}>
-                        <label>Username:</label>
-                    </div>
-                    <input type="text" name="username" id="username" value={form.username} onChange={handleChange}/>
-                </div>
-                <div className={style.formDiv}>
-                    <div className={style.label}>
-                        <label>Email:</label>
-                    </div>
-                    <input type="text" name="email" id="email" value={form.email}/>
-                </div>
-            </form>
+        <div className={style.formTitle}>
+            <h1 onClick={handleLogin} className={login ? style.titleActive : style.inactiveTitle}>Log in</h1>
+            <h2>|</h2>
+            <h1 onClick={handleRegister} className={!login ? style.titleActive : style.inactiveTitle}>Register</h1>
         </div>
+        {
+            login ? (
+                <form className={style.registerForm}>
+                    <div className={style.formDiv}>
+                        <div className={style.label}>
+                            <label>Username:</label>
+                        </div>
+                        <input type="text" name="username" id="username" value={form.username} onChange={handleChange}/>
+                    </div>
+                    <div className={style.formDiv}>
+                        <div className={style.label}>
+                            <label>Email:</label>
+                        </div>
+                        <input type="text" name="email" id="email" />
+                    </div>
+                </form>
+            ) : (
+                <form className={style.registerForm}>
+                    <div className={style.formDiv}>
+                        <div className={style.label}>
+                            <label>Username:</label>
+                        </div>
+                        <input type="text" name="username" id="username" value={form.username} onChange={handleChange}/>
+                    </div>
+                    <div className={style.formDiv}>
+                        <div className={style.label}>
+                            <label>Email:</label>
+                        </div>
+                        <input type="text" name="email" id="email" />
+                    </div>
+                </form>
+            )
+        }
       </div>
     );
 };
