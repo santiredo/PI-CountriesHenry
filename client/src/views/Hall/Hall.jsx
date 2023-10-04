e1a374647c313d540e0b47de628dab29e91a47c6
santiRedondo <santi16redo@gmail.com>
2023-10-03 21:28:26 -0300

    Continuacion de 0Auth de google + estructura basica de formularios de registro y login

--- client/src/views/Hall/Hall.jsx
+++ client/src/views/Hall/Hall.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { gapi } from 'gapi-script'
 import GoogleLogin from 'react-google-login'
 import style from './hall.module.css';
@@ -6,6 +7,19 @@ import style from './hall.module.css';
 export default function Hall () {
 
     const clientID = '295685867400-74qe6ef395rqe5f6nbnlse6dkq505qmv.apps.googleusercontent.com'
+
+    const [userData, setUserData] = useState({})
+
+    const navigate = useNavigate()
+
+    const onSuccess = async(response) => {
+        setUserData(response.profileObj)
+        navigate('/home')
+    }
+
+    const onFailure = (response) => {
+        console.log(response)
+    }
 
     useEffect(() => {
         const start = () => {
@@ -17,17 +31,37 @@ export default function Hall () {
         gapi.load('client:auth2', start)
     }, [])
 
-    const [form, setForm] = useState({
+    // AQUI MANEJAMOS LA DATA DE LOGIN
+
+    const [loginData, setLoginData] = useState({
         username: '',
-        email: ''
+        password: ''
     })
 
-    const handleChange = (event) => {
-        setForm({
-            ...form,
+    const loginChange = (event) => {
+        setLoginData({
+            ...loginData,
             [event.target.name]: event.target.value
         })
     }
+
+    //AQUI MANEJAMOS LA DATA DEL REGISTER
+
+    const [registerData, setRegisterData] = useState({
+        username: '',
+        email: '',
+        password: '',
+        repeatedPassword: ''
+    })
+
+    const registerChange = (event) => {
+        setRegisterData({
+            ...registerData,
+            [event.target.name]: event.target.value
+        })
+    }
+
+    // Aca manejamos los estilos de los formularios
 
     const [login, setLogin] = useState(true)
 
@@ -46,39 +80,39 @@ export default function Hall () {
             <h2>|</h2>
             <h1 onClick={handleRegister} className={!login ? style.titleActive : style.inactiveTitle}>Register</h1>
         </div>
-        {
-            login ? (
-                <form className={style.registerForm}>
-                    <div className={style.formDiv}>
-                        <div className={style.label}>
-                            <label>Username:</label>
-                        </div>
-                        <input type="text" name="username" id="username" value={form.username} onChange={handleChange}/>
-                    </div>
-                    <div className={style.formDiv}>
-                        <div className={style.label}>
-                            <label>Email:</label>
-                        </div>
-                        <input type="text" name="email" id="email" />
-                    </div>
+        <div className={style.bothLoginRegister}>
+            <div className={login ? style.loginDiv : style.hiddenDiv}>
+                <form className={style.loginForm}>
+                    <input type="text" name="username" value={loginData.username} onChange={loginChange} placeholder='Username'/>
+                    <input type="password" name="password" value={loginData.password} onChange={loginChange} placeholder='Password'/>
+                    <button className={style.submit}> Submit </button>
                 </form>
-            ) : (
+                <h4>----- Or -----</h4>
+                <GoogleLogin
+                    className={style.googleLogin}
+                    clientId={clientID}
+                    onSuccess={onSuccess}
+                    onFailure={onFailure}
+                    cookiePolicy={`single_host_policy`}
+                />
+            </div>
+            <div className={!login ? style.registerDiv : style.hiddenDiv}>
                 <form className={style.registerForm}>
-                    <div className={style.formDiv}>
-                        <div className={style.label}>
-                            <label>Username:</label>
-                        </div>
-                        <input type="text" name="username" id="username" value={form.username} onChange={handleChange}/>
-                    </div>
-                    <div className={style.formDiv}>
-                        <div className={style.label}>
-                            <label>Email:</label>
-                        </div>
-                        <input type="text" name="email" id="email" />
-                    </div>
+                    <input type="text" name="username" value={registerData.username} onChange={registerChange} placeholder='Username'/>
+                    <input type="password" name="password" value={registerData.password} onChange={registerChange} placeholder='Password'/>
+                    <input type="password" name="repeatedPassword" value={registerData.repeatedPassword} onChange={registerChange} placeholder='Repeat password'/>
+                    <button className={style.submit}> Submit </button>
                 </form>
-            )
-        }
+                <h4>----- Or -----</h4>
+                <GoogleLogin
+                    className={style.googleRegister}
+                    clientId={clientID}
+                    onSuccess={onSuccess}
+                    onFailure={onFailure}
+                    cookiePolicy={`single_host_policy`}
+                />
+            </div>
+        </div>
       </div>
     );
 };
