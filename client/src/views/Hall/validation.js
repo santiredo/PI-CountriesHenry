e3f0202025b3ceffdf0072b485baeb6a464e8240
santiRedondo <santi16redo@gmail.com>
2023-10-08 20:12:25 -0300

    Creando validadores de login y register para el front

--- client/src/views/Hall/validation.js
+++ client/src/views/Hall/validation.js
@@ -1,19 +1,47 @@
 export const validateRegister = ({username, email, password, repeatedPassword}) => {
 
-    let errors = false
+    let errors = {
+        error: false,
+        username: false,
+        email: '',
+        password: '',
+        repeatedPassword: ''
+    }
 
+    const emailRegex = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+
+    if(!username || username.length < 5){
+        errors.error = true,
+        errors.username = true
+    }
+    if(!emailRegex.test(email) || !email){
+        errors.error = true,
+        errors.email = 'Write a valid email'
+    }
+    if(!password){
+        errors.error = true,
+        errors.password = 'Fill this field'
+    }
     if(password !== repeatedPassword){
-        errors = true
+        errors.error = true,
+        errors.repeatedPassword = 'Both passwords must match'
     }
 
     return errors
 }
 
 export const validateLogin = ({email, password}) => {
-    let errors = false
+    
+    let errors = {
+        error: false,
+        email: '',
+        password: ''
+    }
+    
+    const emailRegex = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
 
-    !email && (errors = true)
-    !password && (errors = true)
+    (!email || !emailRegex.test(email)) && (errors.error = true, errors.email = 'Write a valid email')
+    !password && (errors.error = true, errors.password = 'Fill this field')
 
     return errors
 }
