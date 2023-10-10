3fa6d4f4ceff4616ed9de92b6570a9ad7562dc93
santiRedondo <santi16redo@gmail.com>
2023-10-09 21:43:39 -0300

    Agregamos el icono para el logout y la logica

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -3,16 +3,22 @@ import {NavLink} from 'react-router-dom';
 import home from '../../assets/home.png'
 import create from '../../assets/create.png'
 import activities from '../../assets/activities.png'
+import logOut from '../../assets/logOut.png'
 import style from './nav.module.css'
 
 
 export default function() {
+
+    const handleLogout = () => {
+        localStorage.clear()
+    }
 
     return (
         <div className={style.nav}>
             <NavLink to={`/home`}><img src={home} alt="" /><p>Home</p></NavLink>
             <NavLink to={'/activities'}><img src={activities} alt="" /><p>Activities</p></NavLink>
             <NavLink to={`/form`}><img src={create} alt="" /><p>Create</p></NavLink>
+            <NavLink onClick={handleLogout} to={`/`}><img src={logOut} alt="" /><p>Out</p></NavLink>
         </div>
     )
 }
