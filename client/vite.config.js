7a050e40656379f0a1ce5b4ca5946ba30ff4f586
santiRedondo <santi16redo@gmail.com>
2023-10-13 14:56:51 -0300

    Modificando el archivo vite.config para el deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: 'localhost',
-    port: 5173
-  }
+    proxy: {
+      '/api': 'https://countries-pi-production-8b3a.up.railway.app',
+    },
+  },
 })
