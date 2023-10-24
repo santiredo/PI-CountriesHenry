206a791c339c61e2233047e194c331429d83ba57
santiRedondo <santi16redo@gmail.com>
2023-10-24 18:19:15 -0300

    Agregando configuracion de compilacion a la configuracion de vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Esta línea indica la carpeta de salida
+  },
   server: {
     host: '0.0.0.0'
   }
